6ba3252514030bf70f2b0fd5e5afb80eebb21d12
tuureilmarinen <tuure.ilmarinen@helsinki.fi>
2016-02-01 23:14:02 +0200

    leiska1

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -2,8 +2,8 @@ CREATE TABLE users (
 	id SERIAL PRIMARY KEY,
 	username varchar,
 	password_digest varchar,
-	created_at datetime NOT NULL,
-	updated_at datetime NOT NULL
+	created_at timestamp NOT NULL,
+	updated_at timestamp NOT NULL
 );
 CREATE TABLE tasks (
 	id SERIAL PRIMARY KEY,
@@ -12,20 +12,20 @@ CREATE TABLE tasks (
 	completed boolean,
 	priority integer,
 	user_id integer,
-	created_at datetime NOT NULL,
-	updated_at datetime NOT NULL
+	created_at timestamp NOT NULL,
+	updated_at timestamp NOT NULL
 );
 CREATE TABLE groups (
 	id SERIAL PRIMARY KEY,
 	name varchar,
 	user_id integer,
-	created_at datetime NOT NULL,
-	updated_at datetime NOT NULL
+	created_at timestamp NOT NULL,
+	updated_at timestamp NOT NULL
 );
 CREATE TABLE task_to_groups (
 	id SERIAL PRIMARY KEY,
 	task_id integer,
 	group_id integer,
-	created_at datetime NOT NULL,
-	updated_at datetime NOT NULL
+	created_at timestamp NOT NULL,
+	updated_at timestamp NOT NULL
 );
