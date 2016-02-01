29e65c3a5f8f4dbc1ee64e2d3a0f2e5cf40d6c60
tuureilmarinen <tuure.ilmarinen@helsinki.fi>
2016-02-01 22:22:00 +0200

    fixed create tables for postresqlv2

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,12 +1,12 @@
 CREATE TABLE users (
-	id INTEGER PRIMARY KEY SERIAL NOT NULL,
+	id SERIAL PRIMARY KEY,
 	username varchar,
 	password_digest varchar,
 	created_at datetime NOT NULL,
 	updated_at datetime NOT NULL
 );
 CREATE TABLE tasks (
-	id INTEGER PRIMARY KEY SERIAL NOT NULL,
+	id SERIAL PRIMARY KEY,
 	name varchar,
 	description text,
 	completed boolean,
@@ -16,14 +16,14 @@ CREATE TABLE tasks (
 	updated_at datetime NOT NULL
 );
 CREATE TABLE groups (
-	id INTEGER PRIMARY KEY SERIAL NOT NULL,
+	id SERIAL PRIMARY KEY,
 	name varchar,
 	user_id integer,
 	created_at datetime NOT NULL,
 	updated_at datetime NOT NULL
 );
 CREATE TABLE task_to_groups (
-	id INTEGER PRIMARY KEY SERIAL NOT NULL,
+	id SERIAL PRIMARY KEY,
 	task_id integer,
 	group_id integer,
 	created_at datetime NOT NULL,
