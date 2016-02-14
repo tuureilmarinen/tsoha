8e3e097f2df04f44784aa3c56ce1cbefce4f85ae
tuureilmarinen <tuure.ilmarinen@helsinki.fi>
2016-02-14 21:41:48 +0200

    heroku redirect

--- index.php
+++ index.php
@@ -1,41 +1,44 @@
 <?php
-header("locaton: http://iltu.users.cs.helsinki.fi/tsoha/".$_SERVER[REQUEST_URI]);
+if($_SERVER['HTTP_HOST']=="iltutsoha.herokuapp.com"){
+  header("locaton: http://iltu.users.cs.helsinki.fi/tsoha/".$_SERVER[REQUEST_URI]);
+  die("redirect");
+}
   // Laitetaan virheilmoitukset näkymään
-  error_reporting(E_ALL);
-  ini_set('display_errors', '1');
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
 
   // Selvitetään, missä kansiossa index.php on
-  $script_name = $_SERVER['SCRIPT_NAME'];
-  $explode =  explode('/', $script_name);
+$script_name = $_SERVER['SCRIPT_NAME'];
+$explode =  explode('/', $script_name);
 
-  if($explode[1] == 'index.php'){
-    $base_folder = '';
-  }else{
-    $base_folder = $explode[1];
-  }
+if($explode[1] == 'index.php'){
+  $base_folder = '';
+}else{
+  $base_folder = $explode[1];
+}
 
   // Määritetään sovelluksen juuripolulle vakio BASE_PATH
-  define('BASE_PATH', '/' . $base_folder);
+define('BASE_PATH', '/' . $base_folder);
 
   // Luodaan uusi tai palautetaan olemassaoleva sessio
-  if(session_id() == '') {
-    session_start();
-  }
+if(session_id() == '') {
+  session_start();
+}
 
   // Asetetaan vastauksen Content-Type-otsake, jotta ääkköset näkyvät normaalisti
-  header('Content-Type: text/html; charset=utf-8');
+header('Content-Type: text/html; charset=utf-8');
 
   // Otetaan Composer käyttöön
-  require 'vendor/autoload.php';
+require 'vendor/autoload.php';
 
-  $routes = new \Slim\Slim();
+$routes = new \Slim\Slim();
 //  $routes->add(new \Zeuxisoo\Whoops\Provider\Slim\WhoopsMiddleware);
 
-  $routes->get('/tietokantayhteys', function(){
-    DB::test_connection();
-  });
+$routes->get('/tietokantayhteys', function(){
+  DB::test_connection();
+});
 
   // Otetaan reitit käyttöön
-  require 'config/routes.php';
+require 'config/routes.php';
 
-  $routes->run();
+$routes->run();
