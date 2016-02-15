cb5631a7fbb48bc866ce6bf567658562d5c854e2
tuureilmarinen <tuure.ilmarinen@helsinki.fi>
2016-02-15 19:37:08 +0200

    sessionfix2

--- app/controllers/session_controller.php
+++ app/controllers/session_controller.php
@@ -2,7 +2,6 @@
 require 'app/models/user.php';
 class SessionController extends BaseController{
 	public static function store(){
-		session_start();
 		$user=User::authenticate($_POST['user'],$_POST['password']);
 		if($user){
 			$_SESSION['user']=$user->id;
@@ -11,7 +10,6 @@ class SessionController extends BaseController{
 		View::make('session/new.html', array('errors' => array("Wrong password or username.")));
 	}
 	public static function destroy(){
-		session_start();
 		$_SESSION['user']=null;
 	}
 	public static function create(){
