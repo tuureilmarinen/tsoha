7b596ac208ddeaa79cb0f414fbc86f7a0792859c
tuureilmarinen <tuure.ilmarinen@helsinki.fi>
2016-02-23 00:17:52 +0200

    groupList

--- app/models/group.php
+++ app/models/group.php
@@ -9,7 +9,7 @@ class Group extends BaseModel{
 	}
 	public function find_tasks(){
 		$r=array();
-		$query=DB::connection()->prepare("SELECT t.* FROM tasks AS t INNER JOIN ON task_to_groups AS tg ON tg.task_id=t.id INNER JOIN groups AS g ON tg.group_id=g.id WHERE g.id = :group_id");
+		$query=DB::connection()->prepare("SELECT t.* FROM tasks AS t INNER JOIN task_to_groups AS tg ON tg.task_id=t.id INNER JOIN groups AS g ON tg.group_id=g.id WHERE g.id = :group_id");
 		$query->execute(array('group_id' => $this->id));
 		while($row = $query->fetch()){
 			$r[]=new Task($row);	
