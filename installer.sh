09f6f04af839a7b4d548e296ee6d934eac36d0e1
Tuure Ilmarinen <tuure.ilmarinen@helsinki.fi>
2016-03-01 14:15:04 +0200

    sql

--- installer.sh
+++ installer.sh
@@ -6,4 +6,5 @@ psql < sql/drop_tables.sql &
 psql < sql/create_tables.sql &
 echo "Initalized database" &
 psql < "insert into users values(DEFAULT,'admin','aR//VSL//j5Q6',now(),now(),true);" &
-echo "Created first user: admin password: admin\nRemember to destroy this user when it is not needed anymore"
+echo "Created first user: admin password: admin\nRemember to destroy this user when it is not needed anymore" &
+php composer.phar dump-autoload
